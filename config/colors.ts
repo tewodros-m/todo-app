6e2f7466483f266ef27550acbdaa860130f80bf2
tewodros-m <tewodrosmengie@gmail.com>
2025-01-04 11:19:47 +0300

    define colors

---
+++ config/colors.ts
@@ -0,0 +1,14 @@
+export default {
+  primary: '#E31FE3',
+  primaryDark: '#BC0BBC',
+  primaryMedium: '#E761E7',
+  primaryMediumLight: '#F1B6F1',
+  primaryLight: '#FEE8FE',
+  grey: ' #808080',
+  greyDark: '#2F2F2F',
+  greyDarkMedium: '#525151',
+  greyLight: '#DED9DE',
+  greyMedium: '#BEBEBE',
+  white: '#fff',
+  black: '#000',
+};
