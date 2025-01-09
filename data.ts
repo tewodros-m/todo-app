a89192336d92458ad304017a59c3fd2c867a4e30
tewodros-m <tewodrosmengie@gmail.com>
2025-01-09 11:50:55 +0300

    mock tasks

---
+++ data.ts
@@ -0,0 +1,23 @@
+export const mockTasks = [
+  {
+    id: '1',
+    title: 'Buy groceries',
+    description: 'Milk, Eggs, Bread',
+    createdAt: '12/31/2024',
+    isCompleted: false,
+  },
+  {
+    id: '2',
+    title: 'Call John',
+    description: 'Discuss the project details',
+    createdAt: '01/01/2025',
+    isCompleted: true,
+  },
+  {
+    id: '3',
+    title: 'Finish report',
+    description: 'Complete and submit the financial report',
+    createdAt: '01/02/2025',
+    isCompleted: false,
+  },
+];
